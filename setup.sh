cbeff1d993c859e980ca49416eeb22f4176da406
Jimmie Goode <jimmiegoode@gmail.com>
2014-04-24 00:42:18 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,8 +12,8 @@ cd ../
 # remove old and link new
 rm -rf ~/.starcluster/plugins
 rm -rf ~/.starcluster/config
-ln -s qc_cli/plugins ~/.starcluster/plugins
-ln -s qc_cli/config ~/.starcluster/config
+ln -s quantcluster/plugins ~/.starcluster/plugins
+ln -s quantcluster/config ~/.starcluster/config
 
 ##
 ## <TODO> Prompt user for AWS credentials, then save them to the SC config file
