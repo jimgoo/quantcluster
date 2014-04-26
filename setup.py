de609df561a7aab42a557ba3848f63f395aa2e90
Jimmie Goode <jimmiegoode@gmail.com>
2014-04-26 00:13:42 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='quantcluster-cli',
     url='http://www.quantcluster.com',
     version='0.0.1',
-    description='A command line interface for QuantClusters.',
+    description='A command line interface for QF cluster-computing on AWS EC2.',
     author='Jimmie Goode',
     author_email='jimmie@quantcluster.com',
     packages=find_packages(),
