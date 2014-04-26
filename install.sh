fbbd078c9218fe5fa95a537a7fc0f38e847358ff
jgoode21 <jimmiegoode@gmail.com>
2014-04-26 02:13:06 -0500

    Added config file for StarCluster

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-sudo pip install 
 
-# Get the CLI interface
+#sudo pip install StarCluster
+
+QC_HOME=/opt
+cd $QC_HOME
 git clone https://github.com/quantcluster/quantcluster.git
-
 cd quantcluster/
 python setup.py develop
-cd ../
+starcluster listpublic
+# >> enter 'q' at prompt
 
 # remove old and link new
-rm -rf ~/.starcluster/plugins
-rm -rf ~/.starcluster/config
-ln -s quantcluster/plugins ~/.starcluster/plugins
-ln -s quantcluster/config ~/.starcluster/config
+ln -s $QC_HOME/quantcluster/quantcluster/config ~/.starcluster/config
+ln -s $QC_HOME/quantcluster/quantcluster/plugins ~/.starcluster/plugins
+
 
 ## <TODO> Prompt user for AWS credentials, then save them to the SC config file
+
+echo "Done"
--- quantcluster/plugins/quant_cluster.py
+++ quantcluster/plugins/quant_cluster.py
@@ -10,28 +10,34 @@ class QuantCluster(clustersetup.ClusterSetup):
     """
     def __init__(self):
         self._pool = None
-        self.pip_pkgs = ['celery[librabbitmq]', 'swigibpy' 'scikit-learn' 'six']
         self.apt_pkgs = ['libpq-dev', 'postgresql' 'postgresql-client']
-
+        self.pip_pkgs = ['celery[librabbitmq]', 'swigibpy' 'scikit-learn' 'six']
+        self.git_pkgs = ['jgoode21/lakehouse', 'jgoode21/zipline']
+        
     @property
     def pool(self):
         if self._pool is None:
             self._pool = threadpool.get_thread_pool(20, disable_threads=False)
         return self._pool
 
-    def apt_install(self, nodes):
-        cmd = 'sudo apt-get install %s ' % (' '.join(self.apt_pkgs))
-        log.info('----> QuantCluster>  %s' cmd)
+    def apt_install(self, node):
+        cmd = 'sudo apt-get install -y %s ' % (' '.join(self.apt_pkgs))
         node.ssh.execute(cmd)        
 
-    def pip_install(self, nodes):
+    def pip_install(self, node):
         cmd = 'sudo pip install --upgrade %s ' % (' '.join(self.pip_pkgs))
-        log.info('----> QuantCluster>  %s' cmd)
         node.ssh.execute(cmd)
+
+    def git_install(self, node):
+        cmd = ''
+        for pkg in self.git_pkjs:
+            cmd += 'cd /mnt && git clone https://%s.git' % pkg
+            node.ssh.execute(cmd)
         
     def run(self, nodes, master, user, user_shell, volumes):
         for node in nodes:
             self.pool.simple_job(self.apt_install, (node,), jobid=node.alias)
         for node in nodes:
             self.pool.simple_job(self.pip_install, (node,), jobid=node.alias)            
-            
+        for node in nodes:
+            self.pool.simple_job(self.git_install, (node,), jobid=node.alias)            
