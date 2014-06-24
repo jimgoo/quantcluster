d625af37df9fad291f3ff39d3a686d85ac1e2178
Jimmie Goode <jimmiegoode@gmail.com>
2014-06-24 14:20:04 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='quantcluster',
-    url='http://www.quantcluster.com',
+    url='http://www.goodeanalytics.com',
     version='0.0.1',
     description='A command line interface for QF cluster-computing on AWS EC2.',
     author='Jimmie Goode',
-    author_email='jimmie@quantcluster.com',
+    author_email='jimmie@goodeanalytics.com',
     packages=find_packages(),
     install_requires=['StarCluster'],
     )
